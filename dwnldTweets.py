cc7e15fcde2a8f390c67bac8e408d1a7249e3bb2
Raj Singh Bhatia <raj14bhatia@gmail.com>
2021-02-09 19:09:43 -0800

    added basic config to scrape tweets

--- dwnldTweets.py
+++ dwnldTweets.py
@@ -1,1 +1,12 @@
 import twint
+
+# configure
+c = twint.Config()
+c.Username = "sreekyshooter"
+c.Store_json = True
+c.Profile_full = True # in case of shadow ban
+
+twint.run.Search(c)
+
+# if __name__ == "__main__":
+# 	twint.run.Search(c)
