bd3ed4b81ebb489dfcc94438d21a7ed3ef59dc87
Raj Singh Bhatia <raj14bhatia@gmail.com>
2021-02-12 17:44:19 -0800

    scrapes tweets from account

--- dwnldTweets.py
+++ dwnldTweets.py
@@ -1,12 +1,38 @@
 import twint
+from tqdm import tqdm
+import csv
 
-# configure
-c = twint.Config()
-c.Username = "sreekyshooter"
-c.Store_json = True
-c.Profile_full = True # in case of shadow ban
+# TODO: determine if tweet is a reply
+def is_reply(tweet):
+	# TODO: remove 
+	return
 
-twint.run.Search(c)
 
-# if __name__ == "__main__":
-# 	twint.run.Search(c)
+# TODO: show tweets after searching
+# TODO: add removal of links, removing hashtags, usertags, links, replies from tweets
+def scrapeTweets(username="sreekyshooter", limit=None, includeReplies=False, includeLinks=False, 
+	removeUserTags=False, removeHashTags=False):
+	config = twint.Config()
+	if limit:
+		assert limit % 10 == 0, "'limit' must be multiple of 10"
+	else: # if no limit given, get limit
+		config.Username = "sreekyshooter"
+ 		# config.Profile_full = True # in case of shadow ban enable this
+		config.Store_object = True # store tweets as json objs
+		config.Hide_output = True # enable to hide output of running twint search
+		# config.Store_csv = True
+#		config.Format = "Tweet id: {id} | Date: {date} | Time: {time} | Tweet: {tweet}"
+		if includeLinks is True:
+			config.Links = "include"
+		else:
+			config.Links = "exclude"
+		
+		twint.run.Search(config)
+		tweets = twint.output.tweets_list
+		limit = len(tweets)
+		print(limit)
+
+
+
+if __name__ == "__main__":
+	scrapeTweets()
